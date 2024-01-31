cfb58c4798a96eb577d60fa2d0ebe10a0618b37a
unknown <branni.bazurto@uisek.edu.ec>
2024-01-30 22:45:57 -0500

    repositorio BackEnd

---
+++ CMS.domain/Categories/Category.cs
@@ -0,0 +1,15 @@
+﻿using CMS.domain.Common;
+
+namespace CMS.domain.Categories
+{
+    public class Category : BaseEntity
+    {
+        public string Nombre { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+
+        public Category()
+        {
+            FechaDeCreacion = DateTime.Now;
+        }
+    }
+}
